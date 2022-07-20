27a0fbbd358da3ae38ca512bd080e32064ca2c93
plexyus <45661686+Speck12@users.noreply.github.com>
2022-07-20 13:32:50 +0200

    init

---
+++ scripts/typed_text.js
@@ -0,0 +1,44 @@
+
+window.addEventListener("DOMMouseScroll", function(e) {
+
+    var scrolled = window.pageYOffset;
+    var elem = document.getElementById("aboutmeheader");
+
+    console.log(scrolled);
+
+    if (scrolled => 600) {
+        elem.classList.add(".hidden");
+    }
+    
+})
+
+
+var typed = new Typed(".auto-type1", {
+    strings: ["^1000 a Software Engineer!^3000", "^1000 an UI/UX-Designer!^3000"],
+    typeSpeed: 140,
+    backSpeed: 50,
+    startDelay: 0,
+    shuffle: false,
+    showCursor: false,
+    loop: true
+})
+
+var typed = new Typed(".auto-type2", {
+    strings: ["^1000About me^5000"],
+    typeSpeed: 340,
+    backSpeed: 200,
+    startDelay: 0,
+    shuffle: false,
+    showCursor: false,
+    loop: true
+})
+
+var typed = new Typed(".auto-type3", {
+    strings: ["^1000Projects^5000"],
+    typeSpeed: 340,
+    backSpeed: 200,
+    startDelay: 0,
+    shuffle: false,
+    showCursor: false,
+    loop: true
+})
